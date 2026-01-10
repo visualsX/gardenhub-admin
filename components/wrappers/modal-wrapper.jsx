4121a51b7c9cb25535ee24bf006a78ec85b511eb
osamajavaid <iosamajavaid@gmail.com>
2026-01-10 11:08:33 +0500

    optimised

--- components/wrappers/modal-wrapper.jsx
+++ components/wrappers/modal-wrapper.jsx
@@ -4,13 +4,13 @@ import { CrossIcon } from '@/lib/const/icons';
 import { Modal } from 'antd';
 import useUiStates from '@/store/useUiStates';
 
-const ModalWrapper = ({ children, width }) => {
+const ModalWrapper = ({ children, width, open, onCancel }) => {
   const { isModalOpen, closeModal } = useUiStates();
 
   return (
     <Modal
-      open={isModalOpen.open}
-      onCancel={() => closeModal(false, null)}
+      open={open ?? isModalOpen.open}
+      onCancel={onCancel || (() => closeModal(false, null))}
       footer={null}
       closeIcon={<CrossIcon className="h-5 w-5 text-gray-400" />}
       width={width}
