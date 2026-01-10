ceabee1a5508c7ff1b37f4475632371446edb110
osamajavaid <iosamajavaid@gmail.com>
2026-01-10 11:10:11 +0500

    new state added

--- store/useUiStates.js
+++ store/useUiStates.js
@@ -14,6 +14,11 @@ const useUiStates = create(
       isDeleteModalOpen: { open: false, data: null },
       openDeleteModal: (open, data) => set({ isDeleteModalOpen: { open: open, data: data } }),
       closeDeleteModal: () => set({ isDeleteModalOpen: { open: false, data: null } }),
+
+      //detail modals states and handler
+      isDetailModalOpen: { open: false, data: null },
+      openDetailModal: (open, data) => set({ isDetailModalOpen: { open: open, data: data } }),
+      closeDetailModal: () => set({ isDetailModalOpen: { open: false, data: null } }),
     }),
     {
       name: 'ui-store', // name of the item in the storage (must be unique)
